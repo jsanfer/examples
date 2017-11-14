91ccc708818a6f76dc7e5523bf11e9204ac14d79
Jesus Santamaria Fernandez <Jesus.SantamariaFernandez@fao.org>
2017-11-14 17:39:31 +0100

    Replacing charon-spring-boot-starter by spring-cloud-starter-zuul

--- servlet/src/main/java/com/example/demo/DemoApplication.java
+++ servlet/src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
+
 public class DemoApplication extends SpringBootServletInitializer {
 
     @Override
